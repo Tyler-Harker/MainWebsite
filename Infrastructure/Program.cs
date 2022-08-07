b5108944c647220099944bb8cd2bfd4e660be029
Tyler Harker <tyler@techfabric.co>
2022-08-06 18:02:35 -0700

    removed duplicate stacks to only have 1 appservice plan to cut on costs.

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -17,14 +17,14 @@ return await Pulumi.Deployment.RunAsync(() =>
     var pulumiConfig = new Pulumi.Config();
     var config = pulumiConfig.RequireObject<TylerHarker.MainWebsite.Infrastructure.Configuration.Config>("config");
 
-    var resourceGroup = new ResourceGroup($"tgh-mainwebsite-{config.EnvironmentAbbreviation}", new Pulumi.AzureNative.Resources.ResourceGroupArgs
+    var resourceGroup = new ResourceGroup($"tgh-mainwebsite", new Pulumi.AzureNative.Resources.ResourceGroupArgs
     {
-        ResourceGroupName = $"tgh-mainwebsite-{config.EnvironmentAbbreviation}"
+        ResourceGroupName = $"tgh-mainwebsite"
     });
 
-    var mainWebsiteAppServicePlan = new Pulumi.AzureNative.Web.AppServicePlan($"tgh-appserviceplan-mainwebsite-{config.EnvironmentAbbreviation}", new Pulumi.AzureNative.Web.AppServicePlanArgs
+    var mainWebsiteAppServicePlan = new Pulumi.AzureNative.Web.AppServicePlan($"tgh-appserviceplan-mainwebsite", new Pulumi.AzureNative.Web.AppServicePlanArgs
     {
-        Name = $"tgh-appserviceplan-mainwebsite-{config.EnvironmentAbbreviation}",
+        Name = $"tgh-appserviceplan-mainwebsite",
         Kind = "app",
         ResourceGroupName = resourceGroup.Name,
         Reserved = true,
@@ -37,9 +37,9 @@ return await Pulumi.Deployment.RunAsync(() =>
         }
     });
 
-    var mainWebsiteAppService = new Pulumi.AzureNative.Web.WebApp($"tgh-mainwebsite-{config.EnvironmentAbbreviation}", new Pulumi.AzureNative.Web.WebAppArgs
+    var mainWebsiteAppServiceStaging = new Pulumi.AzureNative.Web.WebApp($"tgh-mainwebsite-stg", new Pulumi.AzureNative.Web.WebAppArgs
     {
-        Name = $"tgh-mainwebsite-{config.EnvironmentAbbreviation}",
+        Name = $"tgh-mainwebsite-stg",
         ResourceGroupName = resourceGroup.Name,
         ServerFarmId = mainWebsiteAppServicePlan.Id,
         HttpsOnly = true,
@@ -48,38 +48,15 @@ return await Pulumi.Deployment.RunAsync(() =>
             LinuxFxVersion = "DOTNETCORE|6.0"
         },
     });
-
-    //var mainWebsiteAppservice = new Pulumi.AzureNative.Resources.Apps
-
-    //// Create an Azure Resource Group
-    ////var resourceGroup = new ResourceGroup("resourceGroup");
-
-    //// Create an Azure resource (Storage Account)
-    //var storageAccount = new StorageAccount("sa", new StorageAccountArgs
-    //{
-    //    ResourceGroupName = resourceGroup.Name,
-    //    Sku = new SkuArgs
-    //    {
-    //        Name = SkuName.Standard_LRS
-    //    },
-    //    Kind = Kind.StorageV2
-    //});
-
-    //var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    //{
-    //    ResourceGroupName = resourceGroup.Name,
-    //    AccountName = storageAccount.Name
-    //});
-
-    //var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    //{
-    //    var firstKey = accountKeys.Keys[0].Value;
-    //    return Output.CreateSecret(firstKey);
-    //});
-
-    //// Export the primary key of the Storage Account
-    //return new Dictionary<string, object?>
-    //{
-    //    ["primaryStorageKey"] = primaryStorageKey
-    //};
+    var mainWebsiteAppServiceProduction = new Pulumi.AzureNative.Web.WebApp($"tgh-mainwebsite-prd", new Pulumi.AzureNative.Web.WebAppArgs
+    {
+        Name = $"tgh-mainwebsite-prd",
+        ResourceGroupName = resourceGroup.Name,
+        ServerFarmId = mainWebsiteAppServicePlan.Id,
+        HttpsOnly = true,
+        SiteConfig = new Pulumi.AzureNative.Web.Inputs.SiteConfigArgs
+        {
+            LinuxFxVersion = "DOTNETCORE|6.0"
+        },
+    });
 });
